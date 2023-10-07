00a57aec845a1d573b36b22c11fd003fe3039585
Daniel Philipi <daniphilipi379@gmail.com>
2023-10-06 23:18:14 -0300

    Feat: Centralizar e menu

--- src/app/page.js
+++ src/app/page.js
@@ -3,27 +3,27 @@ import styles from "../styles/pages/app.module.css";
 export default function Home() {
   return (
     <>
-      <div className={styles.menu}>
-        <div className={styles.op1}>
-          <h1>Opção 1</h1>
-        </div>
-        <div className={styles.op2}>
-          <h1>Opção 2</h1>
-        </div>
-        <div className={styles.logo}>
-        </div>
-        <div className={styles.op3}>
-          <h1>Opção 3</h1>
-        </div>
-        <div className={styles.op4}>
-          <h1>Opção 4</h1>
+      <div className={styles.navegation}>
+        <div className={styles.menu}>
+          <div className={styles.op1}>
+            <h1>História</h1>
+          </div>
+          <div className={styles.separador} />
+          <div className={styles.op2}>
+            <h1>Sobre</h1>
+          </div>
+          <div className={styles.logo}></div>
+          <div className={styles.op3}>
+            <h1>Doação</h1>
+          </div>
+          <div className={styles.separador} />
+          <div className={styles.op4}>
+            <h1>Contatos</h1>
+          </div>
         </div>
       </div>
-      <div className={styles.imagem}>
-        <h1>Imagem</h1>
-      </div>
+      <div className={styles.imagem}/>
       <div className={styles.container}>
-        <h1>Container</h1>
         <div className={styles.anuncios}>
           <h1>Anúncios</h1>
         </div>
@@ -38,7 +38,21 @@ export default function Home() {
         </div>
       </div>
       <div className={styles.footer}>
-        <h1>Footer</h1>
+        <div className={styles.info}>
+
+        </div>
+        <div className={styles.info}>
+
+        </div>
+        <div className={styles.info}>
+
+        </div>
+        <div className={styles.info}>
+
+        </div>
+        <div className={styles.info}>
+
+        </div>
       </div>
     </>
   );
