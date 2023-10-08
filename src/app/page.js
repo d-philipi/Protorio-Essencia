bfabc0af0f68dc3d6a0d7863e969b9df1c3aae08
Daniel Philipi <daniphilipi379@gmail.com>
2023-10-08 09:42:28 -0300

    Feat: Footer e button

--- src/app/page.js
+++ src/app/page.js
@@ -38,21 +38,55 @@ export default function Home() {
         </div>
       </div>
       <div className={styles.footer}>
-        <div className={styles.info}>
-
-        </div>
-        <div className={styles.info}>
-
-        </div>
-        <div className={styles.info}>
-
-        </div>
-        <div className={styles.info}>
-
-        </div>
-        <div className={styles.info}>
-
-        </div>
+        <ul className={styles.info}>
+          <div className={styles.imagemFooter}/>
+        </ul>
+        <ul className={styles.info}>
+          <li>
+            <h2>História</h2>
+          </li>
+          <li>
+            <h2>Liderança</h2>
+          </li>
+          <li>
+            <h2>Houses</h2>
+          </li>
+          <li>
+            <h2>Ministérios</h2>
+          </li>
+          <li>
+            <h2>Agenda</h2>
+          </li>
+        </ul>
+        <ul className={styles.info}>
+          <li>
+            <h1>Doação</h1>
+          </li>
+          <li>
+            <h2>Banco</h2>
+            <h2>Agência:</h2>
+            <h2>Conta:</h2>
+            <h2>CNPJ:</h2>
+          </li>
+          <li>
+            <div className={styles.button}>Doação</div>
+          </li>
+          <li>
+            <h1>Pix</h1>
+          </li>
+          <li>
+            <h2>Chave:</h2>
+            <h1>--</h1>
+          </li>
+        </ul>
+        <ul className={styles.info}>
+          <li>
+            <h1>QrCode para Pix</h1>
+          </li>
+        </ul>
+        <ul className={styles.info}>
+          
+        </ul>
       </div>
     </>
   );
